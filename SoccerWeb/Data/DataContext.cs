7a4d20b782cc2be5317ac217d11bccd7ef33cbe1
KeNsHiN <KeNsHiN@CESAR-PC>
2020-04-06 22:30:23 -0500

    Create DBContext and InitialDB

---
+++ SoccerWeb/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoccerBet.Web.Data.Entity;
+
+namespace SoccerBet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+
+}
---
+++ SoccerWeb/Data/Entity/TeamEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace SoccerBet.Web.Data.Entity
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; }
+    }
+}
